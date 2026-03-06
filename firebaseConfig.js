d0198da87bbf131b73a30550ce7455caf6b0e812
iiisevenxxx <iiisevenxxx@gmail.com>
2026-03-06 14:35:15 +0530

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,4 +1,5 @@
-// firebaseConfig.js
+// Firebase config for Nearby News & Alert System
+
 export const firebaseConfig = {
   apiKey: "AIzaSyAidKMs67_gh4xNLQXiNyDwE4AbhrbvZ7M",
   authDomain: "nearby-news-alert-system.firebaseapp.com",
